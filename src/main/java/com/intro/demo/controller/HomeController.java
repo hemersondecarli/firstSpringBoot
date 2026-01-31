b721fbfbb7e403527847c8896ab745df419e0478
hemersondecarli <hemersondc96@gmail.com>
2026-01-31 23:28:09 +0000

    for debugging purpose

--- src/main/java/com/intro/demo/controller/HomeController.java
+++ src/main/java/com/intro/demo/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.intro.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/index")
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @RequestMapping("/")
     public String index() {
+        System.out.println(appName);
         return "index.html";
     }
 }
