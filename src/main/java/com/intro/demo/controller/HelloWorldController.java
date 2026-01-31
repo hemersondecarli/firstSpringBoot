f01a62fae0ff28ebbb8aebaccbbf351952eb6618
hemersondecarli <hemersondc96@gmail.com>
2026-01-31 05:39:43 +0000

    Instead of making a constructor, there also the @Autowired annotation that can be used to inject that dependency

--- src/main/java/com/intro/demo/controller/HelloWorldController.java
+++ src/main/java/com/intro/demo/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.intro.demo.controller;
 
 import com.intro.demo.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
+    @Autowired
     private HelloWorldService helloWorldService;
-
-    //constructor
-    public HelloWorldController(HelloWorldService helloWorldService) {
-        this.helloWorldService = helloWorldService;
-    }
-
 
     @GetMapping
     public String helloWorld() {
