220d74334229855aa3f1808118f7572b5faa966f
hemersondecarli <hemersondc96@gmail.com>
2026-01-31 22:43:19 +0000

    adjusted to call the html class

---
+++ src/main/java/com/intro/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.intro.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/index")
+    public String index() {
+        return "index.html";
+    }
+}
