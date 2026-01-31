1381ad27f9cf1356d78467418966c7101d782e5e
hemersondecarli <hemersondc96@gmail.com>
2026-01-31 05:37:49 +0000

    Constructor

--- src/main/java/com/intro/demo/controller/HelloWorldController.java
+++ src/main/java/com/intro/demo/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.intro.demo.controller;
 
+import com.intro.demo.service.HelloWorldService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
+    private HelloWorldService helloWorldService;
+
+    //constructor
+    public HelloWorldController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
+
     @GetMapping
     public String helloWorld() {
-        return "Hello World";
+        return helloWorldService.sayHello("Heme");
     }
 }
