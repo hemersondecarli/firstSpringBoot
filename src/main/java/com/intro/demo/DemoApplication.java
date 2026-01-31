89bac14aa10e56776d9ad2761582b524d7a5544e
hemersondecarli <hemersondc96@gmail.com>
2026-01-31 23:27:39 +0000

    Running console app to test injection dependency

--- src/main/java/com/intro/demo/DemoApplication.java
+++ src/main/java/com/intro/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.intro.demo;
 
+import com.intro.demo.service.OrderService;
+import com.intro.demo.service.StripePaymentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+        //spring app
+		//SpringApplication.run(DemoApplication.class, args);
+
+        //running console app
+        var orderService = new OrderService(new StripePaymentService());
+        orderService.placeOrder();
 	}
 
 }
