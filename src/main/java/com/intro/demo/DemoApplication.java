11701ce418012e7f83d64ce106e87a7dea6d85ff
hemersondecarli <hemersondc96@gmail.com>
2026-02-01 23:21:11 +0000

    Bean

--- src/main/java/com/intro/demo/DemoApplication.java
+++ src/main/java/com/intro/demo/DemoApplication.java
@@ -5,22 +5,23 @@ import com.intro.demo.service.PaypalPaymentService;
 import com.intro.demo.service.StripePaymentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
         //spring app
-		//SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+
 
         //running console app
-        var orderService = new OrderService();
+        var orderService = context.getBean(OrderService.class);
 
-        orderService.setPaymentService(new StripePaymentService());
+        //orderService.setPaymentService(new StripePaymentService());
         orderService.placeOrder();
 
-        orderService.setPaymentService(new PaypalPaymentService());
+        //orderService.setPaymentService(new PaypalPaymentService());
         orderService.placeOrder();
 	}
 
